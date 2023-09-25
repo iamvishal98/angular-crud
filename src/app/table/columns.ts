290aae87cb09c2a1ebf29d71798e9f87a7dfb761
Vishal Chauhan <chauhanvishal1998@yahoo.com>
2023-09-25 15:28:56 +0530

    angular-tables

--- src/app/table/columns.ts
+++ src/app/table/columns.ts
@@ -35,41 +35,41 @@ export const columns = [
     title: 'IMAGE-1',
     data: 'image1',
   },
-  // {
-  //   title: 'IMAGE-2',
-  //   data: 'image2',
-  // },
-  // {
-  //   title: 'IMAGE-3',
-  //   data: 'image3',
-  // },
-  // {
-  //   title: 'IMAGE-4',
-  //   data: 'image4',
-  // },
-  // {
-  //   title: 'IMAGE-5',
-  //   data: 'image5',
-  // },
-  // {
-  //   title: 'IMAGE-6',
-  //   data: 'image6',
-  // },
-  // {
-  //   title: 'IMAGE-7',
-  //   data: 'image7',
-  // },
-  // {
-  //   title: 'IMAGE-8',
-  //   data: 'image8',
-  // },
+  {
+    title: 'IMAGE-2',
+    data: 'image2',
+  },
+  {
+    title: 'IMAGE-3',
+    data: 'image3',
+  },
+  {
+    title: 'IMAGE-4',
+    data: 'image4',
+  },
+  {
+    title: 'IMAGE-5',
+    data: 'image5',
+  },
+  {
+    title: 'IMAGE-6',
+    data: 'image6',
+  },
+  {
+    title: 'IMAGE-7',
+    data: 'image7',
+  },
+  {
+    title: 'IMAGE-8',
+    data: 'image8',
+  },
 
-  // {
-  //   title: 'IMAGE-9',
-  //   data: 'image9',
-  // },
-  // {
-  //   title: 'IMAGE-10',
-  //   data: 'image10',
-  // },
+  {
+    title: 'IMAGE-9',
+    data: 'image9',
+  },
+  {
+    title: 'IMAGE-10',
+    data: 'image10',
+  },
 ];
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -11,65 +11,77 @@ export class TableComponent implements OnInit {
   constructor(private apiService: ApiService, private http: HttpClient) {}
   allData: any = [];
   dtOptions: DataTables.Settings = {};
-  // dataTablesParameters = { scrollY: 400}
 
-  // someClickHandler(info: any): void {
-  //   console.log(info)
-  // }
+  visible: boolean = false;
+  label: string= '';
+  value: string='';
+
+  Close(): void {
+    this.visible = false;
+  }
+  Open(data: any, colind: number): void {
+    this.visible=true;
+    this.label = columns[colind].title
+    this.value=data;
+  }
 
   ngOnInit(): void {
     this.dtOptions = {
-      ajax: (dataTablesParameters: any, callback : any) => {
-        this.apiService.fetchData().subscribe((resp : any) => {
-          //  const formattedData= resp.data.map((item : any) => {
-          //     return {
-          //       'id': item.id,
-          //       'email':item.email,
-          //       'name':item.name,
-          //       'dob':item.dob,
-          //       'postode':item.postode,
-          //       'street':item.street,
-          //       'telephone':item.telephone,
-          //       'town':item.town,
-          //       'image1':`<img class='Image-tab' src='${item.image1}' alt='image-1'/>`
-          //     }
-          //   })
-            callback({
-              recordsTotal: resp.data.length,
-              recordsFiltered: resp.data.length,
-              data: resp.data
+      ajax: (dataTablesParameters: any, callback: any) => {
+        this.http
+          .get('../assets/demo-data.json', dataTablesParameters)
+          .subscribe((resp: any) => {
+            //console.log(Math.floor((Date.now() / 1000) % 60));
+            const formattedData = resp.data.map((item: any) => {
+              return {
+                id: `<a class="itemId">${item.id}</a>`,
+                textId: item.id,
+                email: item.email,
+                name: `<a>${item.name}</a>`,
+                textName: item.name,
+                dob: item.dob,
+                postode: item.postode,
+                street: item.street,
+                telephone: item.telephone,
+                town: item.town,
+                image1: `<img class='Image-tab' src='${item.image1}' alt='image-1'/>`,
+                image2: `<img class='Image-tab' src='${item.image2}' alt='image-1'/>`,
+                image3: `<img class='Image-tab' src='${item.image3}' alt='image-1'/>`,
+                image4: `<img class='Image-tab' src='${item.image4}' alt='image-1'/>`,
+                image5: `<img class='Image-tab' src='${item.image5}' alt='image-1'/>`,
+                image6: `<img class='Image-tab' src='${item.image6}' alt='image-1'/>`,
+                image7: `<img class='Image-tab' src='${item.image7}' alt='image-1'/>`,
+                image8: `<img class='Image-tab' src='${item.image8}' alt='image-1'/>`,
+                image9: `<img class='Image-tab' src='${item.image9}' alt='image-1'/>`,
+                image10: `<img class='Image-tab' src='${item.image10}' alt='image-1'/>`,
+              };
             });
+            callback({
+              recordsTotal: formattedData.length,
+              recordsFiltered: formattedData.length,
+              data: formattedData,
+            });
+
+            //console.log(Math.floor((Date.now() / 1000) % 60));
           });
       },
-     columns,
-     scrollX:true,
-     searching:true,
-    //  rowCallback: (row: Node, data: any , index: number) => {
-    //   const self = this;
-    //   // Unbind first in order to avoid any duplicate handler
-    //   // (see https://github.com/l-lin/angular-datatables/issues/87)
-    //   // Note: In newer jQuery v3 versions, `unbind` and `bind` are 
-    //   // deprecated in favor of `off` and `on`
-    //   // $('td', row).off('click');
-    //   // $('td', row).on('click', () => {
-    //   //   self.someClickHandler(data);
-    //   //   //console.log(row)
-    //   // });
-    //   // return row;
-    //   //console.log(data.image1)
-    //   $('td:eq(9)', row).html( `<img src='${data.image1}' alt='image-1'/>` );
-    //   // $('td:eq(9)', row).DataTable({createdRow: () => {
+      columns,
+      scrollX: true,
+      deferRender: true,
+      rowCallback: (row: Node, data: any, index: number) => {
+        const self = this;
+        $('.itemId', row).on('click', () => {
+          this.Open(data.textId,0);
+        });
+        $('td:eq(2)', row).on('click', () => {
+         this.Open(data.textName,2);
+        });
+        // return row;
+        // $('td:eq(9)', row).html( `<img src='${data.image1}' alt='image-1'/>` );
+        // $('td:eq(9)', row).DataTable({createdRow: () => {
 
-    //   // }})
-    // },
-    
-    }
-  }
-
-  fetchAllData(): void {
-    this.apiService.fetchData().subscribe((response: any) => {
-      this.allData = response.data;
-      //console.log(this.allData)
-    });
+        // }})
+      },
+    };
   }
 }
